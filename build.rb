77a893abfe9d756c4de6597e81b386eeec438840
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-04-26 15:58:21 -0700

    Add name

--- build.rb
+++ build.rb
@@ -5,9 +5,9 @@ FileUtils.mkdir("build")
 File.open("build/index.html", "w") do |file|
   file << %{<!doctype html>
 <html>
-<head><title>Hello</title></head>
+<head><title>Hello #{ENV['NAME']}</title></head>
 <body>
-  <h1>Hello</h1>
+  <h1>Hello #{ENV['NAME']}</h1>
 </body>
 </html>}
 end
