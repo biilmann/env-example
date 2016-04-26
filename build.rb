b924ed9d0a379228ce19210b2ee8225b507047a0
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-04-26 16:01:54 -0700

    Add site url

--- build.rb
+++ build.rb
@@ -8,6 +8,8 @@ File.open("build/index.html", "w") do |file|
 <head><title>Hello #{ENV['NAME']}</title></head>
 <body>
   <h1>Hello #{ENV['NAME']}</h1>
+
+  <p>This site is at <a href="#{ENV['URL']}">#{URL}</a></p>
 </body>
 </html>}
 end
