53d454d0877784c3ce11b85400e50d84625dfaba
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-04-26 16:07:01 -0700

    Make it for aaron

--- build.rb
+++ build.rb
@@ -7,7 +7,7 @@ File.open("build/index.html", "w") do |file|
 <html>
 <head><title>Hello #{ENV['NAME']}</title></head>
 <body>
-  <h1>Hello #{ENV['NAME']}</h1>
+  <h1>#{if ENV['NAME'] == 'Aaron' then 'Hello there Aaron!' else 'This is a tutorial for Aaron...' end}</h1>
 
   <p>This site is at <a href="#{ENV['URL']}">#{ENV['URL']}</a></p>
 </body>
